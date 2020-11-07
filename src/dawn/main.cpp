4c0eab88132387c06733c9a14bc59fe94d0eccd4
Scott Crawford <dogbird9@gmail.com>
2020-11-07 16:36:45 -0500

    added up/down movement to the cam

--- src/dawn/main.cpp
+++ src/dawn/main.cpp
@@ -155,6 +155,10 @@ void processInput(GLFWwindow *window)
         camera.ProcessKeyboard(LEFT, *deltaTime);
     if (glfwGetKey(window, GLFW_KEY_D) == GLFW_PRESS)
         camera.ProcessKeyboard(RIGHT, *deltaTime);
+    if (glfwGetKey(window, GLFW_KEY_SPACE) == GLFW_PRESS)
+        camera.ProcessKeyboard(UP, *deltaTime);
+    if (glfwGetKey(window, GLFW_KEY_LEFT_SHIFT) == GLFW_PRESS)
+        camera.ProcessKeyboard(DOWN, *deltaTime);
     //timimng changes
     if (glfwGetKey(window, GLFW_KEY_0) == GLFW_PRESS)
         Time->set_time_multipler(0);
